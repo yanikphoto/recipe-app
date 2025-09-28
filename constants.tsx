844cd94ce2bc44a3284fd6726d51acc73d8e5a49
yanikphoto <161635808+yanikphoto@users.noreply.github.com>
2025-09-28 11:08:02 -0400

    Add files via upload

---
+++ constants.tsx
@@ -0,0 +1,14 @@
+export const DEFAULT_CATEGORIES = [
+  'Déjeuner',
+  'Soupes',
+  'Salades',
+  'Pâtes',
+  'Porc',
+  'Viande rouge',
+  'Poisson',
+  'Dessert',
+  'Végétarien',
+  'Poulet',
+  'Boeuf',
+  'Boisson',
+];
