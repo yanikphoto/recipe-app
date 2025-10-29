cf18bd0d0af934d2346d265aeeb68e86472831bf
yanikphoto <161635808+yanikphoto@users.noreply.github.com>
2025-10-29 10:20:44 -0400

    Add files via upload

--- components/SearchModal.tsx
+++ components/SearchModal.tsx
@@ -93,10 +93,10 @@ const SearchModal: React.FC<SearchModalProps> = ({ recipes, onSelectRecipe, onCl
                 <div>
                     {showResults ? (
                         filteredRecipes.length > 0 ? (
-                            <ul className="space-y-3">
+                            <ul className="space-y-2">
                                 {filteredRecipes.map(recipe => (
                                     <li key={recipe.id} onClick={() => onSelectRecipe(recipe)} className="flex items-center gap-4 bg-white p-3 rounded-xl cursor-pointer hover:bg-gray-50 transition-colors">
-                                        <div className="w-1/3 flex-shrink-0">
+                                        <div className="w-5/12 flex-shrink-0">
                                             <StoredImage imageId={recipe.imageUrl} alt={recipe.title} className="w-full aspect-square rounded-lg object-cover"/>
                                         </div>
                                         <div className="flex-1">
