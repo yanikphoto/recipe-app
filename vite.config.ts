2af6da51f1296a7f3ff6d4449f37207757736dd4
yanikphoto <161635808+yanikphoto@users.noreply.github.com>
2025-12-27 15:42:05 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  },
   build: {
     rollupOptions: {
+      // This tells Vite not to bundle these libraries, as they are provided 
+      // via the <script type="importmap"> in index.html
       external: [
         'react',
         'react-dom',
