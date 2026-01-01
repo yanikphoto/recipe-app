90801009abe23ae9b0c4305541b0b3622fcf4ba4
yanikphoto <161635808+yanikphoto@users.noreply.github.com>
2026-01-01 09:21:45 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,23 @@
-
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    }
-  },
-  build: {
-    rollupOptions: {
-      // This tells Vite not to bundle these libraries, as they are provided 
-      // via the <script type="importmap"> in index.html
-      external: [
-        'react',
-        'react-dom',
-        'react-dom/client',
-        '@google/genai',
-        'buffer',
-        'docx'
-      ]
-    }
-  }
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
+      plugins: [react()],
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
 });
